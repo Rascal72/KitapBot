ccd9d675a4f83effa93888935562999d55a9ed27
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-19 22:28:30 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -13,7 +13,7 @@ import shutil
 import time
 from datetime import datetime
 
-from config import DOWNLOAD_LOCATION, LOG_CHANNEL, HTTP_PROXY, TG_MAX_FILE_SIZE, DEF_WATER_MARK_FILE, PROMO, PRE_LOG, userbot, BOT_PM, OWNER_ID
+from config import DOWNLOAD_LOCATION, LOG_CHANNEL, HTTP_PROXY, TG_MAX_FILE_SIZE, DEF_WATER_MARK_FILE, PROMO, PRE_LOG, userbot, BOT_PM, OWNER_ID, MOLY_LINKLERI
 
 from pyrogram.enums import MessageEntityType, ChatAction
 from database.database import db
@@ -286,6 +286,11 @@ async def yt_dlp_call_back(bot, update):
     if yt_dlp_password is not None:
         command_to_exec.append("--password")
         command_to_exec.append(yt_dlp_password)
+    if MOLY_LINKLERI != 0:
+        for ref in MOLY_LINKLERI:
+            if f"{ref}" in yt_dlp_url:
+            command_to_exec.append("--referer")
+            command_to_exec.append("https://vidmoly.to/")
     LOGGER.info(command_to_exec)
     start = datetime.now()
     start1 = time.time() 
