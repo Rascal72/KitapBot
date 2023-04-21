25d8b61cc271674a214f7949ed15f619bb014286
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-21 13:10:56 +0300

    Update temiz.py

--- plugins/temiz.py
+++ plugins/temiz.py
@@ -1,11 +1,18 @@
 from pyrogram import Client, filters
 from sys import executable
 import os
+import asyncio
 
 @Client.on_message(filters.command('onbellek'))
 async def onbellek(bot, message):
     try:
-        os.execl(executable, executable, "onbellek.sh")
+        command = ["bash","onbellek.sh"]
+        process = await asyncio.create_subprocess_exec(
+            *command,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            )
         await message.reply_text("Önbellek Silindi..")
     except Exception as e:
         await message.reply_text(e)
