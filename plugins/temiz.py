a6de822c5f3106fdd734c0090d7206eff2f6634a
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-21 13:22:20 +0300

    Update temiz.py

--- plugins/temiz.py
+++ plugins/temiz.py
@@ -2,21 +2,15 @@ from pyrogram import Client, filters
 from sys import executable
 import os
 import asyncio
+from streamlit import caching
 
 @Client.on_message(filters.command('onbellek'))
 async def onbellek(bot, message):
     try:
         msg = await message.reply_text("Önbellek Siliniyor..") 
-        command = ["bash","onbellek.sh"]
-        process = await asyncio.create_subprocess_exec(
-            *command,
-            # stdout must a pipe to be accessible as process.stdout
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-            )
-        if process.returncode == 0:
-            await msg.edit('Önbellek Silindi..')
-        else:
-            await msg.edit('Önbellek Silinemedi..')
+        try:
+            caching.clear_cache()
+        except Exception as e:
+            await msg.edit(f"Önbellek Silinmedi..\n{e}")
     except Exception as e:
         await message.reply_text(e)
