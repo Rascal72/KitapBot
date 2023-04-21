22da7e8f46ffaf1b5aa5a87cfb6f228c94fdcb89
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-21 13:15:12 +0300

    Update temiz.py

--- plugins/temiz.py
+++ plugins/temiz.py
@@ -6,6 +6,7 @@ import asyncio
 @Client.on_message(filters.command('onbellek'))
 async def onbellek(bot, message):
     try:
+        msg = await message.reply_text("Önbellek Siliniyor..") 
         command = ["bash","onbellek.sh"]
         process = await asyncio.create_subprocess_exec(
             *command,
@@ -13,6 +14,14 @@ async def onbellek(bot, message):
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
             )
-        await message.reply_text("Önbellek Silindi..")
+        await asyncio.wait([
+            read_stderr(start,msg, process),
+            process.wait(),
+        ])
+    
+        if process.returncode == 0:
+            await msg.edit('Önbellek Silindi..)
+        else:
+            await msg.edit('Önbellek Silinemedi..')
     except Exception as e:
         await message.reply_text(e)
