76ae0ec864f4196a59faf815cdbb8ac717fcdf08
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-21 13:52:43 +0300

    Update temizle.py

--- plugins/temizle.py
+++ plugins/temizle.py
@@ -11,6 +11,7 @@ LOGGER = logging.getLogger(__name__)
 
 async def dosyasil(dosyaYolu, message, textim):
     for dosya in os.listdir(dosyaYolu):
+        text = "DOWNLOADS"
         dosyaYolu = os.path.join(text, dosya)
         try:
             if os.path.isfile(dosyaYolu):
