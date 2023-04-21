fac9309be066108b9d0b5047a77f0ca9bf11548f
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-21 13:02:52 +0300

    Update temiz.py

--- plugins/temiz.py
+++ plugins/temiz.py
@@ -5,7 +5,7 @@ import os
 @Client.on_message(filters.command('onbellek'))
 async def onbellek(bot, message):
     try:
-        os.execl(executable, executable, "onbelleksil.sh")
+        os.execl(executable, executable, "onbellek.sh")
         await message.reply_text("Önbellek Silindi..")
     except Exception as e:
         await message.reply_text(e)
