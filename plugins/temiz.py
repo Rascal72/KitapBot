66b3ad131df6eb6222b79ffb75c039511f04670d
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-04-21 13:02:14 +0300

    Create temiz.py

---
+++ plugins/temiz.py
@@ -0,0 +1,11 @@
+from pyrogram import Client, filters
+from sys import executable
+import os
+
+@Client.on_message(filters.command('onbellek'))
+async def onbellek(bot, message):
+    try:
+        os.execl(executable, executable, "onbelleksil.sh")
+        await message.reply_text("Önbellek Silindi..")
+    except Exception as e:
+        await message.reply_text(e)
